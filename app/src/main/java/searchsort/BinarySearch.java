953700a3681d31cdda587223eaacb9b172d31db2
jamie wang <bombyjamie@gmail.com>
2024-12-06 17:23:37 -0500

    did binary search class

--- app/src/main/java/searchsort/BinarySearch.java
+++ app/src/main/java/searchsort/BinarySearch.java
@@ -7,7 +7,24 @@ public class BinarySearch extends Search {
      */
     @Override
     public int find(int[] arr, int target) {
-        return 0;
+        int low = 0; 
+        int high = arr.length - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2; 
+
+            if (arr[mid] == target) {
+                return mid; 
+            } 
+            else if (arr[mid] < target) {
+                low = mid + 1; 
+            } 
+            else {
+                high = mid - 1; 
+            }
+        }
+
+        return -1; 
     }
 
     /**
@@ -16,7 +33,21 @@ public class BinarySearch extends Search {
      */
     @Override
     public int recursiveFind(int[] arr, int target) {
-        return 0;
+        return binarySearchHelper(arr, target, 0, arr.length - 1);
     }
+    private static int binarySearchHelper(int[] arr, int target, int low, int high) {
+        if (low > high) {
+            return -1;
+        }
+
+        int mid = low + (high - low) / 2;
+
+        if (arr[mid] == target) {
+            return mid; 
+        } else if (arr[mid] < target) {
+            return binarySearchHelper(arr, target, mid + 1, high);
+        } else {
+            return binarySearchHelper(arr, target, low, mid - 1);
+        }
 
 }
